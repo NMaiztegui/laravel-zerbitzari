beb193a854c6fc7b11ea4eded58f80e055892d0b
Naroa Maiztegui <naroamaiztegui@gmail.com>
2024-12-13 13:23:21 +0100

    laravel start

---
+++ ProiektuaZerbitzari/app/Models/character.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class character extends Model
+{
+    //
+    protected $fillable = [
+        'actor',
+        'name',
+        'description',
+        ];
+}
