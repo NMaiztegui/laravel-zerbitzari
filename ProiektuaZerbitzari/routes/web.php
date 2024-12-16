7c02396bd6e72709cb384f3b8e716d38d628d715
Naroa Maiztegui <naroamaiztegui@gmail.com>
2024-12-16 12:18:44 +0100

    oinarrizko autentifikazioa

--- ProiektuaZerbitzari/routes/web.php
+++ ProiektuaZerbitzari/routes/web.php
@@ -6,3 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/', 'App\Http\Controllers\CharacterController@show');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
